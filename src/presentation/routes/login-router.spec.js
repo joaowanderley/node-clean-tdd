a0b20bd0fd17f270c68962e5ef15b5bc78b3128f
joao <joaowannderley@hotmail.com>
2020-09-23 09:39:27 -0300

    add tests if email or password not provided

---
+++ src/presentation/routes/login-router.spec.js
@@ -0,0 +1,33 @@
+class LoginRouter {
+  route (httpRequest) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+describe('Login Router', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httResponse = sut.route(httpRequest)
+    expect(httResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no password is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com'
+      }
+    }
+    const httResponse = sut.route(httpRequest)
+    expect(httResponse.statusCode).toBe(400)
+  })
+})
