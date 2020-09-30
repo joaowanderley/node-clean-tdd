3aa3a036384195bc920f5e38fa704481d6a31077
joao <joaowannderley@hotmail.com>
2020-09-30 08:41:25 -0300

    covereged test

--- src/presentation/routes/login-router.js
+++ src/presentation/routes/login-router.js
@@ -26,7 +26,6 @@ module.exports = class LoginRouter {
       }
       return HttpResponse.ok({ accessToken })
     } catch (error) {
-      // console.log(error)
       return HttpResponse.serverError()
     }
   }
